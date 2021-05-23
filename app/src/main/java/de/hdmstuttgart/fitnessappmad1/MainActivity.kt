daed1e550306450cf63108dbcfa0f2a19660cb5e
js414 <js414@hdm-stuttgart.de>
2021-05-23 19:51:37 +0200

    initial

---
+++ app/src/main/java/de/hdmstuttgart/fitnessappmad1/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.hdmstuttgart.fitnessappmad1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
